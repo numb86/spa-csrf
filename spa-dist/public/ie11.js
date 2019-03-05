bbfcbc942481bbc4793f4ec0a01ea93d4183bc70
numb86 <86numb@gmail.com>
2019-03-06 00:31:32 +0900

    汎用化した。

--- spa-dist/public/ie11.js
+++ spa-dist/public/ie11.js
@@ -4,19 +4,29 @@
     elem.innerHTML = apiResponse.result;
   };
 
-  var xhr = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        retouchDom(JSON.parse(xhr.responseText));
+  function apiRequest(method, endPoint, body, onSuccess) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          onSuccess(JSON.parse(xhr.responseText));
+        }
       }
-    }
+    };
+
+    xhr.open(method, endPoint);
+    xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send(body ? JSON.stringify(body) : null);
   };
 
-  xhr.open('POST', 'http://localhost:8010/cookie/login');
-  xhr.withCredentials = true;
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  xhr.send(JSON.stringify({userName: 'numb', password: 'pass'}));
+  apiRequest(
+    'POST',
+    'http://localhost:8010/cookie/login',
+    {userName: 'numb', password: 'pass'},
+    retouchDom
+  );
 })();
